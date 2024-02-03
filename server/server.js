d997065bc695b223796702c6899ad1e74537146f
pradeept <pradeep.tarakar17@gmail.com>
2024-02-03 19:19:45 +0530

    add docker filesand docker-compose file

--- server/server.js
+++ server/server.js
@@ -1,18 +1,24 @@
-require('dotenv').config()
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const todoRouter = require('./router/todos')
-const userRouter = require('./router/user')
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const todoRouter = require("./router/todos");
+const userRouter = require("./router/user");
 
-const app  = express();
+const app = express();
 
 app.use(express.json());
 app.use(cors());
-mongoose.connect(process.env.MONGO_URI);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("connected to Mongodb");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
+app.use("/", todoRouter);
+app.use("/user", userRouter);
 
-app.use('/',todoRouter);
-app.use('/user', userRouter);
-
-app.listen(process.env.PORT,()=>console.log('Alive on 3001'));
+app.listen(process.env.PORT, () => console.log(`Alive on ${process.env.PORT}`));
